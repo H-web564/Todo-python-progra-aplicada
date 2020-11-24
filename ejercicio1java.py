fcce820ff9e026466318d12876d470940423d10a
H-web564 <74988179+H-web564@users.noreply.github.com>
2020-11-24 14:59:35 -0500

    Add files via upload

---
+++ ejercicio1java.py
@@ -0,0 +1,8 @@
+#Anthony Buenaño
+print("ingrese numero")
+n1 = float(input())
+print("ingrese numero")
+n2 = float(input())
+mul = n1*n2
+print("su resultado de la multiplicacion",n1,"x",n2,"es",mul)
+
